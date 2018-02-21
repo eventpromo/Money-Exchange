2178408f1b100fd460eb26ab4498f14a634bcb0a
Siarhei <eventpromoby@gmail.com>
2018-02-21 22:11:55 +0300

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency == 0){
+        return {};
+    }
+    if(currency > 10000 ){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    var nominates = {
+        "H": 50,
+        "Q": 25,
+        "D": 10,
+        "N": 5,
+        "P": 1
+    }
+    var result = {};
+    for(let nominate in nominates){
+        if(currency == 0){
+            break;
+        }
+        var count = Math.floor(currency / nominates[nominate]);
+        if(count > 0){
+            result[nominate] = count;
+        }
+        currency -= nominates[nominate] * count;
+    }
+    return result;
 }
